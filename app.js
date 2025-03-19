898a0c74267043546bb46ba378a76a6b63c77b48
Aqsa <al.aqsarw@gmail.com>
2025-03-20 00:51:28 +0700

    add format date function

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-// API
-// Daily: weather code, max-temp, uv index
-// Curent weather: temp, weather code, wind speed, rain, showers, isDayNight, surface pressure
+/* DOM Selector */
+
+const cityInput = document.getElementById('city-input')
+
+const daily = document.getElementById('daily');
+const cityDisplay = document.getElementById('city-display');
+const date = document.getElementById('date');
+const minTemp = document.getElementById('min-temp');
+const maxtemp = document.getElementById('max-temp');
+const weatherDesc = document.getElementById('weather-desc');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const uvIndex = document.getElementById('uv-index');
+const airPressure = document.getElementById('air-pressure')
+
+
+/*
+API
+Daily: weather code, min-temp, max-temp, uv index
+Curent weather: temp, weather code, wind speed, isDayNight, surface pressure, relative humidity
+*/
+
+const formattedDate = (date) => {
+    const options = {
+        weekday: 'long'  // Only keep weekday option
+    };
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString('en-US', options);
+};
 
 function getApiUrl(latitude, longitude) {
-    return `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,uv_index_max&current=temperature_2m,weather_code,wind_speed_10m,surface_pressure,showers,rain,is_day&timezone=auto`
+    return `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,uv_index_max,temperature_2m_min&current=temperature_2m,weather_code,wind_speed_10m,surface_pressure,is_day,relative_humidity_2m&timezone=auto`
 }
 
 function getLocation() {
@@ -13,7 +39,7 @@ function getLocation() {
                 (position) => {
                     const latitude = position.coords.latitude;
                     const longitude = position.coords.longitude;
-                    console.log ({latitude, longitude})
+                    // console.log ({latitude, longitude}) udah bisa
                     resolve({ latitude, longitude });
                 },
                 (error) => {
@@ -28,16 +54,14 @@ function getLocation() {
     });
 }
 
-function getWeatherData(apiUrl) {
-    return fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Weather data fetched successfully");
-            return data;
-        })
-        .catch(error => {
-            throw new Error(`Error fetching weather data: ${error}`);
-        });
+async function getWeatherData(apiUrl) {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(`Error fetching weather data: ${error}`);
+    }
 }
 
 function initWeatherApp() {
@@ -49,6 +73,7 @@ function initWeatherApp() {
         .then(data => {
             // Process and display the weather data
             console.log(data);
+            date.innerText = formattedDate(data.current.time);
         })
         .catch(error => console.error(error));
 }
