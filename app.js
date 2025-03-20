bb2c77e2f59cc05f6502355f8fca01316163240e
Aqsa <al.aqsarw@gmail.com>
2025-03-20 11:51:03 +0700

    current weather: temperature, day, min-temp, max-temp, humidity, windspeed. week weather: short date, temperature

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const cityInput = document.getElementById('city-input')
 const daily = document.getElementById('daily');
 const cityDisplay = document.getElementById('city-display');
 const date = document.getElementById('date');
+const currTemp = document.getElementById('temperature');
 const minTemp = document.getElementById('min-temp');
-const maxtemp = document.getElementById('max-temp');
+const maxTemp = document.getElementById('max-temp');
 const weatherDesc = document.getElementById('weather-desc');
 const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
@@ -23,6 +24,14 @@ Curent weather: temp, weather code, wind speed, isDayNight, surface pressure, re
 const formattedDate = (date) => {
     const options = {
         weekday: 'long'  // Only keep weekday option
+    };
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString('en-US', options);
+};
+
+const formattedShortDate = (date) => {
+    const options = {
+        weekday: 'short'
     };
     const newDate = new Date(date);
     return newDate.toLocaleDateString('en-US', options);
@@ -74,6 +83,34 @@ function initWeatherApp() {
             // Process and display the weather data
             console.log(data);
             date.innerText = formattedDate(data.current.time);
+            temperature.innerText = data.current.temperature_2m;
+            temperature.innerText += data.current_units.temperature_2m;
+            humidity.innerText = data.current.relative_humidity_2m + data.current_units.relative_humidity_2m;
+            windSpeed.innerText = data.current.wind_speed_10m + data.current_units.wind_speed_10m;
+            minTemp.innerText = `Min Temperature - ${data.daily.temperature_2m_min[0]} ${data.daily_units.temperature_2m_min}`;
+            maxTemp.innerText = `Max Temperature - ${data.daily.temperature_2m_max[0]} ${data.daily_units.temperature_2m_max}`;
+            
+            daily.innerHTML = ``
+
+            for (let i = 1; i < data.daily.time.length; i++) {
+                daily.innerHTML +=
+                    `
+                    <div class="col-4 col-sm-3 col-md-auto col-lg">
+                        <div class="card bg-gradient text-white text-center rounded-3 border-0"
+                            style="max-width: 106px; max-height: 176px">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="fs-5">
+                                    ${formattedShortDate(data.daily.time[i])}
+                                </h5>
+                                <img src="windy-sunny-1.png" width="70" height="70" style="margin: 0 auto;" />
+                                <span class="fs-5">${data.daily.temperature_2m_max[i]}${data.daily_units.temperature_2m_max}</span>
+                            </div>
+                        </div>
+                    </div>
+                    `
+            }
+
+
         })
         .catch(error => console.error(error));
 }
